de78961125442c614184a911a71c982e1d9c923f
Maximiliano Pietiewicz <maximilianopietkiewicz04@gmail.com>
2024-10-28 16:11:47 -0300

    wip

--- src/products/components/ButtonForm.tsx
+++ src/products/components/ButtonForm.tsx
@@ -2,24 +2,14 @@ import { Button } from "@/components/shadcn/ui/button";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/shadcn/ui/dialog";
-import { Input } from "@/components/shadcn/ui/input";
-import { Label } from "@/components/shadcn/ui/label";
-import { useState } from "react";
-import { IProduct } from "../interfaces/product.interface";
+
 import { Formulario } from "./Formulario";
 export const ButtonForm = () => {
-  const handleCreateProduct = () => {
-    console.log("handleCreateProduct");
-  };
-
-  const [prodcutForm, setProductForm] = useState<IProduct>();
-
   return (
     <Dialog>
       <DialogTrigger asChild>
